8de8ec46f854873929765e07a4c1d94d83b2974d
perosky168 <ogunbanjopetet10@gmail.com>
2023-12-03 15:02:26 +0100

    refactored code to pass data through context

--- index.js
+++ index.js
@@ -1,43 +1,35 @@
 const path = require('path');
 const express = require('express');
-
 const { ApolloServer } = require('apollo-server-express');
+
 const { loadFilesSync } = require('@graphql-tools/load-files');
 const { makeExecutableSchema } = require('@graphql-tools/schema');
-
 const typesArray = loadFilesSync(path.join(__dirname, '**/*.graphql'));
 
-const { products } = require('./data');
+const { products, categories, reviews } = require('./data');
 
-const resolvers = {
-  Query: {
-    hello: () => {
-      return 'World';
-    },
-    products: () => {
-      return products
-    },
-    product: (parent, args, context) => {
-      const productId = args.id;
-
-      const product = products.find(product => product.id === productId);
-      if(!product) return null
-      else return product
-    },
-  },
-};
+const { Query } = require('./resolvers/Query')
+const { Product } = require('./resolvers/Product')
+const { Category } = require('./resolvers/Category')
 
 async function startApolloServer() {
   const app = express();
 
   const schema = makeExecutableSchema({
     typeDefs: typesArray,
-    resolvers
+    resolvers: {
+      Query,
+      Category,
+      Product,
+    },
   });
 
-
   const server = new ApolloServer({
-    schema
+    schema, context: {
+      categories,
+      products,
+      reviews,
+    }
   });
 
   await server.start();
